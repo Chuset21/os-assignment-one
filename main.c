f644e992841ccee6656e5b1ac9733d159179e311
Miguel Bloggs <arrietam@tcd.ie>
2022-09-25 15:53:35 -0400

    Added jobs and fg command

--- main.c
+++ main.c
@@ -11,6 +11,137 @@
 
 #define ARGS_SIZE 30
 
+
+typedef struct job {
+    char *name;
+    pid_t pid;
+} job;
+
+typedef struct node {
+    job *data;
+    struct node *next;
+} node;
+
+node *head = NULL;
+
+/**
+ * Creates a job
+ * @param name name of the job
+ * @param pid pid of the child
+ * @return pointer to the created job
+ */
+job *createJob(char *const name, pid_t pid) {
+    job *const result = malloc(sizeof(job));
+    result->name = name;
+    result->pid = pid;
+    return result;
+}
+
+/**
+ * Creates a node
+ * @param name name of the job
+ * @param pid pid of the child
+ * @return pointer to the created node
+ */
+node *createNode(char *const name, pid_t pid) {
+    node *const result = malloc(sizeof(node));
+    result->data = createJob(name, pid);
+    result->next = NULL;
+    return result;
+}
+
+/**
+ * Adds job to the linked list
+ * @param name name of the job
+ * @param pid pid of the child
+ */
+void addNode(char *const name, pid_t pid) {
+    node *const temp = createNode(name, pid);
+    if (head == NULL) {
+        head = temp;
+    } else {
+        node *cur = head;
+        while (cur->next != NULL) {
+            cur = cur->next;
+        }
+        cur->next = temp;
+    }
+}
+
+/**
+ * Removes the xth entry from the list
+ * @param x index of entry from the list to be removed
+ * @return NULL if unsuccessful, otherwise the pointer to the removed node
+ */
+node *removeNode(int x) {
+    node *cur = head;
+    node *prev = NULL;
+    for (int i = 1; i < x && cur != NULL; ++i) {
+        prev = cur;
+        cur = cur->next;
+    }
+    if (cur == NULL) {
+        return NULL;
+    }
+
+    if (cur == head) {
+        head = cur->next;
+    } else {
+        prev->next = cur->next;
+    }
+
+    return cur;
+}
+
+/**
+ * Executes the jobs command
+ * @param params parameters for command (should be empty)
+ */
+void jobs(char *params[]) {
+    if (*params != NULL) {
+        fprintf(stderr, "jobs: too many arguments\n");
+        return;
+    }
+
+    int index = 1;
+    for (node *cur = head; cur != NULL; cur = cur->next, ++index) {
+        printf("[%d]\t%s\n", index, cur->data->name);
+    }
+}
+
+/**
+ * Executes the fg command
+ * @param params parameters for command
+ */
+void fg(char *params[]) {
+    int index = 1;
+    if (*params != NULL) {
+        if (params[1] != NULL) {
+            fprintf(stderr, "fg only accepts 1 argument\n");
+            return;
+        } else {
+            index = (int) strtol(*params, NULL, 10);
+        }
+    }
+    if (index < 1) {
+        fprintf(stderr, "fg only accepts positive indexes\n");
+        return;
+    }
+
+    node *const pNode = removeNode(index);
+    if (pNode == NULL) {
+        fprintf(stderr, "fg given invalid index [%d]\n", index);
+        return;
+    }
+
+    int status = 0;
+    waitpid(pNode->data->pid, &status, WUNTRACED);
+
+    free(pNode->data->name);
+    free(pNode->data);
+    free(pNode);
+}
+
 /**
  * Executes the echo command
  * @param params parameters for command
@@ -68,9 +199,11 @@ void exitShell() {
  * @return true if the command was matched and run, false otherwise
  */
 bool isBuiltIn(char *cmd, char *params[]) {
-    if (strcmp(cmd, "cd") == 0) {
+    if (strcmp(cmd, "cd") == 0 || strcmp(cmd, "fg") == 0) {
     } else if (strcmp(cmd, "pwd") == 0) {
         pwd(params);
+    } else if (strcmp(cmd, "jobs") == 0) {
+        jobs(params);
     } else if (strcmp(cmd, "exit") == 0) {
         exitShell();
     } else if (strcmp(cmd, "echo") == 0) {
@@ -206,14 +339,16 @@ void runCmd(char *args[], const char *const outputRedirection) {
 
 /**
  * Use the given command/s
- * @param args command/s
+ * @param command command as a string
+ * @param args command/s (tokenized)
  * @param commandLength the amount of tokens in args
  * @param background whether the command should run in the background
  * @param outputRedirection where the output should be redirected, if NULL no redirection will take place
  * @param cmdPipeIndex the next index after where the pipe was found, if -1 then no command piping will take place
  */
 void
-useCommand(char *args[], int commandLength, bool background, const char *const outputRedirection, int cmdPipeIndex) {
+useCommand(char *const command, char *args[], int commandLength, bool background, const char *const outputRedirection,
+           int cmdPipeIndex) {
     if (commandLength > 0) {
         if (commandLength > ARGS_SIZE) {
             printf("Arguments exceeded max size\n");
@@ -222,9 +357,14 @@ useCommand(char *args[], int commandLength, bool background, const char *const o
             if (childPID) {
                 if (strcmp(*args, "cd") == 0) {
                     cd(args + 1);
+                } else if (strcmp(*args, "fg") == 0) {
+                    fg(args + 1);
                 } else if (!background) {
                     int status = 0;
                     waitpid(childPID, &status, WUNTRACED);
+                    free(command);
+                } else {
+                    addNode(command, childPID);
                 }
             } else {
                 if (cmdPipeIndex > 0) {
@@ -288,8 +428,7 @@ int main(void) {
         char *const buffer = getLine(&bufLen);
         if (buffer != NULL) {
             const int commandLength = getcmd(buffer, bufLen - 1, args, &background, &outputRedirection, &cmdPipeIdx);
-            useCommand(args, commandLength, background, outputRedirection, cmdPipeIdx);
-            free(buffer);
+            useCommand(buffer, args, commandLength, background, outputRedirection, cmdPipeIdx);
         }
     }
 #pragma clang diagnostic pop
